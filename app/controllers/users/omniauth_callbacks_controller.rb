aeafcea97f81ea591e48d14a397ca2f44870c07c
Martin <vuquangthang87@gmail.com>
2014-07-14 09:30:49 +0700

    Update Omniauth callback message

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect @user, event: :authentication
     else
-      redirect_to root_path, notice: 'You are not allowed to log in.'
+      flash[:alert] = 'You are not allowed to log in.'
+      redirect_to root_path
     end
   end
 
