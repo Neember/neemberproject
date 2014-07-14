022afaf07ac7488952497b9ce4fa9ba767c494cb
Martin <vuquangthang87@gmail.com>
2014-07-14 09:31:55 +0700

    Add default scope for Coder

--- app/models/coder.rb
+++ app/models/coder.rb
@@ -2,4 +2,6 @@ class Coder < User
 
   has_and_belongs_to_many :projects, join_table: 'coders_projects'
   has_many :absences, through: :projects
+
+  default_scope -> { where(is_admin: false) }
 end
