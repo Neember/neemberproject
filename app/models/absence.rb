dafd8a186f77c230d9f4ec089c5cc517f5e3b45c
Martin <vuquangthang87@gmail.com>
2014-07-23 09:28:23 +0700

    Refactor AbsenceModel

--- app/models/absence.rb
+++ app/models/absence.rb
@@ -1,5 +1,11 @@
 class Absence < ActiveRecord::Base
-  after_initialize :assigns_default_values
+  default_scope -> { order(date: :desc, id: :desc) }
+
+  delegate :name, to: :project, prefix: true, allow_nil: true
+  delegate :first_name, to: :coder, prefix: true, allow_nil: true
+
+  belongs_to :project
+  belongs_to :coder
 
   validates_presence_of :date
   validates_presence_of :hours
@@ -9,13 +15,7 @@ class Absence < ActiveRecord::Base
   validates_numericality_of :hours, greater_than: 0
   validates_numericality_of :hours, less_than_or_equal_to: 8
 
-  belongs_to :project
-  belongs_to :coder
-
-  delegate :name, to: :project, prefix: true, allow_nil: true
-  delegate :first_name, to: :coder, prefix: true, allow_nil: true
-
-  default_scope -> { order(date: :desc, id: :desc) }
+  after_initialize :assigns_default_values
 
   def assigns_default_values
     self.date ||= Date.today
